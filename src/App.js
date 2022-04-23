50736caf4a0a4720eff64088b04a9e80563e3e4d
Satishcg12 <sung20700@gmail.com>
2022-04-23 21:36:18 +0545

    get started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-
+import { BrowserRouter as Router } from 'react-router-dom'  
+import Header from './components/Header';
 function App() {
   return (
-    <div className="App">
-      
+    <>
+    <Header/>
+      <Router>
+
+      </Router>
     
-    </div>
+    </>
   );
 }
 
