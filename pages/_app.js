4912cffd44fb828bfb96442136352d36b9014867
Satishcg12 <sung20700@gmail.com>
2022-07-23 09:33:32 +0545

    added nav box

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import NavBox from '../components/NavBox'
 import '../styles/globals.css'
 
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div style={{display:'flex' , width:"100vw"}}>
+      
+      <NavBox/>
+      <div style={{width:'50vw', background:'rgb(71, 71, 71)', color:"white"}}>
+
+      <Component {...pageProps} />
+      </div>
+    </div>
+  )
 }
 
 export default MyApp
