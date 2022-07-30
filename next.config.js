039b1ea263e2475a29206f120248fdafb02bc298
Satishcg12 <sung20700@gmail.com>
2022-07-30 16:40:47 +0545

    sa

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withPwa = require('next-pwa')
 module.exports = withPwa({
   pwa:{
     dest: "public",
-    register: true,
+    // register: true,
     skipWaiting: true,
     disable: process.env.NODE_ENV === 'development',
   },
